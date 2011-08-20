57e053997176a240c1b4c648e062ea55a09cc18b
Dan Langevin <dan.langevin@lifebooker.com>
2011-08-20 16:53:44 -0400

    adding list matcher

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -18,12 +18,16 @@ RSpec::Matchers.define(:match_response) do |actual|
 end
 
 # alias as match_json
-RSpec::Matchers.define(:match_json) do |actual|
+RSpec::Matchers.define(:match_list) do |actual|
   
   match do |expected|
+    actual = JSON.parse(actual) if actual.is_a?(String)
+    expected = JSON.parse(expected) if expected.is_a?(String)  
+    actual = actual.first if actual.is_a?(Array)
+    expected = expected.first if expected.is_a?(Array)
     PathString.as_sorted_json(actual) == PathString.as_sorted_json(expected)
   end
-  
+    
   failure_message_for_should do |container|
     "expected #{PathString.as_sorted_json(actual)}\n to equal\n #{PathString.as_sorted_json(container)}"
   end
--- spec/lib/matcher_spec.rb
+++ spec/lib/matcher_spec.rb
@@ -10,4 +10,8 @@ describe "match_resonse Matcher" do
     {:a => {:b => "/test/test"}}.should match_response({"a" =>{"b" => "/:a/:b"}})
   end
   
+  it "should match the first element in a list" do
+    {"a" => ":b"}.should match_list([{"a" => "test"}, {"a" => "test2"}])
+  end
+  
 end
