eb4ea9e267fd9a3dd833ef9306381050f0fe93ba
Dan Langevin <dan.langevin@lifebooker.com>
2011-08-20 16:30:41 -0400

    aliasing matcher to match_json - was conflicting with webrat

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -16,3 +16,20 @@ RSpec::Matchers.define(:match_response) do |actual|
   end
   
 end
+
+# alias as match_json
+RSpec::Matchers.define(:match_json) do |actual|
+  
+  match do |expected|
+    PathString.as_sorted_json(actual) == PathString.as_sorted_json(expected)
+  end
+  
+  failure_message_for_should do |container|
+    "expected #{PathString.as_sorted_json(actual)}\n to equal\n #{PathString.as_sorted_json(container)}"
+  end
+  
+  failure_message_for_should_not do |container|
+    "expected #{PathString.as_sorted_json(actual)}\n not to equal\n #{PathString.as_sorted_json(container)}"
+  end
+  
+end
